26cdf193153054f40a581878cb65d48f77588804
jiashiwei <jiashiwei@fang.com>
2017-05-25 18:08:37 +0800

    这是一个关于Spring web的练习

---
+++ src/test/java/spittr/web/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package spittr.web;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import spittr.web.HomeController;
+
+public class HomeControllerTest {
+
+  @Test
+  public void testHomePage() throws Exception {
+    HomeController controller = new HomeController();
+    InternalResourceViewResolver re = new InternalResourceViewResolver();
+    re.setPrefix("/WEB-INF/views/");
+    re.setSuffix("jsp");
+    MockMvc mockMvc = standaloneSetup(controller).build();
+    mockMvc.perform(get("/home"))
+           .andExpect(view().name("home"));
+  }
+
+}
